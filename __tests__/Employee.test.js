334c3626dd40125fd46cef990166642d53f83c81
Alyssa20lopez <alyssa20lopez@gmail.com>
2022-09-10 19:59:14 -0400

    Created Manager test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -10,29 +10,29 @@ it("should create an object for employess info", () => {
   expect(employee.email).toEqual(expect.any(String));
 });
 
-// get name from getName()
+// finds name from getName()
 it('employee name', () => {
   const employee = new Employee('Alyssa', 1015,'alyssa20lopez@gmail.com');
 
   expect(employee.getName()).toEqual(expect.any(String));
 });
 
-// gets id from getId()
+// finds id from getId()
 it('employee id', () => {
   const employee = new Employee('Alyssa', 1015,'alyssa20lopez@gmail.com');
 
   expect(employee.getId()).toEqual(expect.any(Number));
 });
 
-// gets email from getEmail()
+// finds email from getEmail()
 it('employee email', () => {
   const employee = new Employee('Alyssa', 1015,'alyssa20lopez@gmail.com');
 
   expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
 });
 
-// gets role from getRole()
-it('employee email', () => {
+// finds role from getRole()
+it('employee role', () => {
   const employee = new Employee('Alyssa', 1015,'alyssa20lopez@gmail.com');
 
   expect(employee.getRole()).toEqual("Employee");
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,6 +1,15 @@
 const Manager = require("../lib/Manager");
-// team manager’s name, employee ID, email address, and office number
-describe("Manager", () => {
 
+// team manager’s name, employee ID, email address, and office number
+it("should create a Manager object", () => {
+  const manager = new Manager('Alyssa', 1015,'alyssa20lopez@gmail.com', 1);
+  
+  expect(manager.officeNum).toEqual(expect.any(Number));
 });
 
+// finds role from getRole()
+it('employee role', () => {
+  const manager = new manager('Alyssa', 1015,'alyssa20lopez@gmail.com');
+
+  expect(manager.getRole()).toEqual("Manager");
+});
