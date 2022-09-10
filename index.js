4c729293e96080a47fa3be064f8af644947c94cd
Alyssa20lopez <alyssa20lopez@gmail.com>
2022-09-10 09:45:24 -0400

    Set Up Func for Index.js

--- index.js
+++ index.js
@@ -5,5 +5,19 @@ const Intern = require('./lib/Intern');
 // TODO: CODE GOES HERE
 
 const manager = new Manager();
+const emp1 = "[]";
+const inform = manager.inform(emp1)
+
+console.log(emp1, "->", inform)
+
 const engineer = new Engineer();
+const emp2 = "[]";
+const inform = engineer.inform(emp2)
+
+console.log(emp2, "->", inform)
+
 const intern = new Intern
+const emp3 = "[]";
+const inform = intern.inform(emp3)
+
+console.log(emp3, "->", inform)
